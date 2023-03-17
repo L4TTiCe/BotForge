c2a94ff087f549bc9cf3e09f61ec330e2e963fe9
Dheshan Mohandass <dheshan@mohandass.com>
2023-03-16 20:04:57 -0400

    feat.: Impl. Dynamic Theme

--- app/src/main/java/com/mohandass/botforge/ui/theme/Theme.kt
+++ app/src/main/java/com/mohandass/botforge/ui/theme/Theme.kt
@@ -1,10 +1,16 @@
 package com.mohandass.botforge.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -72,19 +78,62 @@ private val DarkColors = darkColorScheme(
     scrim = md_theme_dark_scrim,
 )
 
+//@Composable
+//fun BotForgeTheme(
+//    useDarkTheme: Boolean = isSystemInDarkTheme(),
+//    content: @Composable() () -> Unit
+//) {
+//    val systemUiController = rememberSystemUiController()
+//
+//    systemUiController.setSystemBarsColor(
+//        color = Color.Transparent
+//    )
+//
+//    val colors = if (!useDarkTheme) {
+//        LightColors
+//    } else {
+//        DarkColors
+//    }
+//
+//    MaterialTheme(
+//        colorScheme = colors,
+//        content = content
+//    )
+//}
+
 @Composable
 fun BotForgeTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    useDynamicColor: Boolean = true,
+    content: @Composable () -> Unit,
 ) {
-    val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
+    val colorScheme = when {
+        useDynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val insets = WindowCompat.getInsetsController(window, view)
+            window.statusBarColor = Color.Transparent.toArgb()
+            window.navigationBarColor = Color.Transparent.toArgb()
+            insets.isAppearanceLightStatusBars = !useDarkTheme
+            insets.isAppearanceLightNavigationBars = !useDarkTheme
+        }
     }
 
     MaterialTheme(
-        colorScheme = colors,
-        content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content,
     )
 }
+
